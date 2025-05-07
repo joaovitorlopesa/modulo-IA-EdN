32236e88793f77646111c74a05c53981cf01382d
João <joaovitorlopes.a@gmail.com>
2025-05-07 11:50:44 -0300

    Atividades da aula 01: idade condicional e preço com desconto

---
+++ aula01/idade-condicional.py
@@ -0,0 +1,9 @@
+#Este código verifica se a idade digitada é maior ou igual a 18.
+
+idade = 15 #Definindo a variável idade com o valor 15.
+
+if idade >= 18: #Se a idade for maior ou igual a 18, exibe "Você é maior de idade.".
+    print("Você é maior de idade.") 
+else: #Se não for, exibe "Você é menor de idade.".
+    print("Você é menor de idade.") 
+#Fim do código.
---
+++ aula01/preco-com-desconto.py
@@ -0,0 +1,8 @@
+#Este código calcula o preço final de um celular com desconto.
+
+preco = 1000 #Preço original do celular.
+desconto = 15 #Desconto em porcentagem.
+
+valor_final = preco - (preco * desconto / 100) #Calculo do valor final com desconto.
+print(f"O preço do celular de Maria com {desconto}% de desconto é R$ {valor_final}") #Exibe o valor final.
+#Fim do código.
